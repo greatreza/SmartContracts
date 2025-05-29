67afa80e2c3fae7db469f481d8c92b45dbe77dc8
greatreza <reza.zeinali21@gmail.com>
2025-05-29 16:17:01 +0330

    ADD limit weet

--- contracts/twitter.sol
+++ contracts/twitter.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 contract twitter{
 
+    uint16 constant MAX_TWEET_LENGTH = 280;
+
     struct Tweet {
         address author;
         string content;
@@ -13,6 +15,8 @@ contract twitter{
     mapping(address => Tweet []) public tweets;
 
     function createTweet(string memory _tweet) public {
+        require(bytes(_tweet).length <= MAX_TWEET_LENGTH , "Tweet is too long!");
+
         Tweet memory newTweet = Tweet({
             author:msg.sender,
             content:_tweet,
